f2b4b364a2e1381c1b70a8dcf1f115f9afc6e6b7
GonzaloTafur <gonzatafurbe@gmail.com>
2026-04-29 20:33:53 -0500

    sin titulo

---
+++ usuario.js
@@ -0,0 +1,59 @@
+export const usuarios = [
+    { 
+        "id": 1,
+        "usuario": "johndoe", 
+        "password": "john123",
+        "dias": [
+            {
+                "dia":"Jueves",
+                "fecha": "23-04-2026",
+                "hinicio": "07:54",
+                "hfin": "18:00",
+                "htotal": "10:54",
+                "monto": "200"
+            },
+            {
+                "dia":"Viernes",
+                "fecha": "24-04-2026",
+                "hinicio": "09:59",
+                "hfin": "17:00",
+                "htotal": "08:59",
+                "monto": "150"
+            },
+            {
+                "dia":"Sabado",
+                "fecha": "25-04-2026",
+                "hinicio": "09:00",
+                "hfin": "17:00",
+                "htotal": "09:00",
+                "monto": "150"
+            },
+            {
+                "dia":"Lunes",
+                "fecha": "27-04-2026",
+                "hinicio": "08:00",
+                "hfin": "18:15",
+                "htotal": "11:15",
+                "monto": "250"
+            },
+            {
+                "dia":"Martes",
+                "fecha": "28-04-2026",
+                "hinicio": "07:56",
+                "hfin": "18:05",
+                "htotal": "10:56",
+                "monto": "175"
+            },
+            {
+                "dia":"Miercoles",
+                "fecha": "29-04-2026",
+                "hinicio": "07:59",
+                "hfin": "18:02",
+                "htotal": "11:01",
+                "monto": "250"
+            }
+        ]
+    },
+
+];
+
